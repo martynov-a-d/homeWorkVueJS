c15c44320e2f91b66f95c641afefffd8631a698f
Alex <lovealex574@gmail.com>
2021-09-29 13:53:47 +0300

    Create component Terminal

---
+++ src/components/Terminal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="table-block">
+      <h3>Таблица расходов</h3>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.table-block {
+  margin: 0 auto;
+  width: 300px;
+  height: 300px;
+  background: lightgrey;
+  color: black;
+}
+</style>
