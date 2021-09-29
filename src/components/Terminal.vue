ff900c0f976c71f41e6cbf0e449d9b764922029f
Alex <lovealex574@gmail.com>
2021-09-29 17:14:19 +0300

    Add logic to Terminal 0.0.1

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,10 +1,46 @@
 <template>
   <div>
     <div class="table-block">
-      <h3>Таблица расходов</h3>
+      <h3>Mypersonal coasts</h3>
+      <button>Add new coast</button>
+      <table>
+        <tr class="title_table">
+          <td class="title_table-elem">Date</td>
+          <td class="title_table-elem">Category</td>
+          <td class="title_table-elem">Value</td>
+        </tr>
+        <tr v-for="elem in purchase" :key="elem.id">
+          <td class="table_elem">{{ elem.date }}</td>
+          <td class="table_elem">{{ elem.name }}</td>
+          <td class="table_elem">{{ elem.price }}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      purchase: [
+        {
+          id: 1,
+          name: "eat",
+          date: "31.07.1990",
+          price: 320,
+        },
+        {
+          id: 2,
+          name: "cafe",
+          date: "31.07.1990",
+          price: 540,
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .table-block {
@@ -13,5 +49,10 @@
   height: 300px;
   background: lightgrey;
   color: black;
+}
+.title_table-elem {
+  width: 100px;
+  height: 100px;
+  text-align: center;
 }
 </style>
