941f20a553b029bbf7e86d9baa8e8b5691a6efb9
Alex <lovealex574@gmail.com>
2021-10-03 22:01:06 +0300

    Fix from hw2

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,21 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <!-- Component calc -->
     <Calculator />
+    <Terminal />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Calculator from "./components/Calculator.vue";
+import Terminal from "./components/Terminal.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     Calculator,
+    Terminal,
   },
 };
 </script>
