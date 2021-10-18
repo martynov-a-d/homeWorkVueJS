3a8128393a5c7e0c7e6911093464ea65cc993acc
Alex <lovealex574@gmail.com>
2021-10-18 20:49:39 +0300

    Connected the router to the app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
 
 
 Vue.config.productionTip = false
@@ -8,4 +9,5 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   store,
+  router,
 }).$mount('#app')
