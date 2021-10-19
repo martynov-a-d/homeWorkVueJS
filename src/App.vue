22d64d7a7da339cff69906c47f887600c4904948
Alex <lovealex574@gmail.com>
2021-10-19 13:37:26 +0300

    Refactoring btn with components to linkWithRouter

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <div id="app">
+    <!-- Component VueRouter for nav -->
+    <header>
+      <router-link to="/helloWorld">HelloWorld</router-link>
+      <router-link to="/calculator">Calculator</router-link>
+      <router-link to="/terminal">Terminal</router-link>
+    </header>
+    <router-view />
     <!-- Component HelloWord -->
-    <button @click="isVisibleBlock('isHidenHW')">Hello World</button>
+
+    <!-- <button @click="isVisibleBlock('isHidenHW')">Hello World</button>
     <div v-show="isHidenHW">
       <img alt="Vue logo" src="./assets/logo.png" />
       <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    </div> -->
+
     <!-- Component calc -->
-    <button @click="isVisibleBlock('isHidenCalc')">Calculator</button>
-    <Calculator v-show="isHidenCalc"/>
+
+    <!-- <button @click="isVisibleBlock('isHidenCalc')">Calculator</button>
+    <Calculator v-show="isHidenCalc" /> -->
+
     <!-- Component term -->
-    <button @click="isVisibleBlock('isHidenTerm')">Terminal</button>
-    <Terminal v-show="isHidenTerm"/>
+
+    <!-- <button @click="isVisibleBlock('isHidenTerm')">Terminal</button>
+    <Terminal v-show="isHidenTerm" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import Calculator from "./components/Calculator.vue";
-import Terminal from "./components/Terminal.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import Calculator from "./components/Calculator.vue";
+// import Terminal from "./components/Terminal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    Calculator,
-    Terminal,
+    // HelloWorld,
+    // Calculator,
+    // Terminal,
   },
   data() {
     return {
@@ -35,24 +47,24 @@ export default {
     };
   },
   methods: {
-    isVisibleBlock (elem) {
-      switch (elem) {
-        case "isHidenHW":
-          this.isHidenHW = ! this.isHidenHW;
-          break;
+    // isVisibleBlock (elem) {
+    //   switch (elem) {
+    //     case "isHidenHW":
+    //       this.isHidenHW = ! this.isHidenHW;
+    //       break;
 
-        case "isHidenCalc":
-          this.isHidenCalc = !this.isHidenCalc;
-          break;
+    //     case "isHidenCalc":
+    //       this.isHidenCalc = !this.isHidenCalc;
+    //       break;
 
-        case "isHidenTerm":
-          this.isHidenTerm = !this.isHidenTerm;
-          break;
+    //     case "isHidenTerm":
+    //       this.isHidenTerm = !this.isHidenTerm;
+    //       break;
 
-        default:
-          break;
-      }
-    }
+    //     default:
+    //       break;
+    //   }
+    // }
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import HelloWorld from '../components/HelloWorld.vue';
+import Calculator from '../components/Calculator.vue';
+import Terminal from '../components/Terminal.vue';
+
 Vue.use(Router)
 
 export default new Router({
-    
+    mode: 'history',
+    routes: [
+        {
+            path: '/helloWorld',
+            name: 'helloWorld',
+            component: HelloWorld,
+        },
+        {
+            path: '/calculator',
+            name: 'calculator',
+            component: Calculator,
+        },
+        {
+            path: '/terminal',
+            name: 'terminal',
+            component: Terminal,
+        }
+    ]
 })
