9a816b6489a31ba47a3d2f1a2a732b595f3ae47a
Alex Martynov <lovealex574@gmail.com>
2021-11-07 21:10:43 +0300

    non work

--- src/store/purchases.js
+++ src/store/purchases.js
@@ -1,26 +1,27 @@
 export default {
     actions: {
-        fetch(url)
-            .then(res => res.json())
-            .then(ctx.commit('setPurchaiseListData', test))
-        // fetchData(ctx) {
-        //     const res = [
-        //       {
-        //         id: 1,
-        //         name: "eat",
-        //         date: "2021-10-17",
-        //         price: 320,
-        //       },
-        //       {
-        //         id: 2,
-        //         name: "cafe",
-        //         date: "2021-10-18",
-        //         price: 540,
-        //       },
-        //     ]
-        //     const test = res;
-        //     ctx.commit('setPurchaseListData', test)
-        // },
+        // url = 'https://raw.githubusercontent.com/lovealex574/homeWorkVueJS/hw_4/dataTranc.json'
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(ctx.commit('setPurchaiseListData', test))
+        fetchData(ctx) {
+            const res = [
+              {
+                id: 1,
+                name: "eat",
+                date: "2021-10-17",
+                price: 320,
+              },
+              {
+                id: 2,
+                name: "cafe",
+                date: "2021-10-18",
+                price: 540,
+              },
+            ]
+            const test = res;
+            ctx.commit('setPurchaseListData', test)
+        },
     },
     mutations: {
         setPurchaseListData(state, purchase) {
