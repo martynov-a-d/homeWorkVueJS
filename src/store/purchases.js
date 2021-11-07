c721f8b9ea1a2b4d319edb6adb104492d21fa54f
Alex Martynov <lovealex574@gmail.com>
2021-11-07 20:49:50 +0300

    Add logic Fetch

--- src/store/purchases.js
+++ src/store/purchases.js
@@ -1,23 +1,26 @@
 export default {
     actions: {
-        fetchData(ctx) {
-            const res = [
-              {
-                id: 1,
-                name: "eat",
-                date: "2021-10-17",
-                price: 320,
-              },
-              {
-                id: 2,
-                name: "cafe",
-                date: "2021-10-18",
-                price: 540,
-              },
-            ]
-            const test = res;
-            ctx.commit('setPurchaseListData', test)
-        },
+        fetch(url)
+            .then(res => res.json())
+            .then(ctx.commit('setPurchaiseListData', test))
+        // fetchData(ctx) {
+        //     const res = [
+        //       {
+        //         id: 1,
+        //         name: "eat",
+        //         date: "2021-10-17",
+        //         price: 320,
+        //       },
+        //       {
+        //         id: 2,
+        //         name: "cafe",
+        //         date: "2021-10-18",
+        //         price: 540,
+        //       },
+        //     ]
+        //     const test = res;
+        //     ctx.commit('setPurchaseListData', test)
+        // },
     },
     mutations: {
         setPurchaseListData(state, purchase) {
